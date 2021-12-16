0eb3fed0762f2b8ec2e340063c3ed0446a59fc59
dgabin <gabindepaire@imac-de-dgabin.home>
2021-12-16 16:18:42 +0100

    Feature : upd seeder cities

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,27 +1,26 @@
 import { PrismaClient } from "@prisma/client";
-import { users } from './users';
+import { seedUsers } from './users';
+import { seedCities } from './cities';
 
 const prisma = new PrismaClient();
 
 async function main() {
-    if (process.env.NODE_ENV !== "developpement") {
-        return;
-    }
 
-    await prisma.city.deleteMany()
-    await prisma.user.deleteMany()
+    await prisma.user.deleteMany({})
+    await prisma.city.deleteMany({})
 
-    await prisma.city.create({
-        data: {
-            'name' : 'Bordeaux',
-            'postalCode' : "33000"
-        }
-    })
-    for (let user of users) {
-        await prisma.user.create({
-            data: user
-        })
-    }
+    // await prisma.city.create({
+    //     data: {
+    //         'name' : 'Bordeaux',
+    //         'postalCode' : "33000"
+    //     }
+    // })
+
+    await seedCities()
+
+    console.log('seeding Users...')
+    await seedUsers()
+
 }
 
 main().catch(e => {
--- prisma/users.ts
+++ prisma/users.ts
@@ -1,13 +1,26 @@
-export const users = [
-    {
-        "firstName" : "Ludo",
-        "lastName" : "Nom",
-        "username" : "pseudotest",
-        "birthDate" : "2018-02-15T00:33:02.000Z",
-        "email" : "exemple@gmail.com",
-        "password" : "12345",
-        "city": { "connect": { "id": 1 } },
-        "sex": 1
-    },
+import * as Faker from 'faker';
+import { City, PrismaClient, User } from "@prisma/client";
+import { CityService } from '../src/user/city/city.service';
+import { random } from 'faker';
+import { CreateUserDto, UserDto } from 'src/dtos';
 
-]
+const prisma = new PrismaClient()
+
+export const seedUsers = async():Promise<void> => {
+    const cities = await prisma.city.findMany()
+    const promises: Promise<User>[] = []
+
+    for(let index = 0; index < 10; index++) {
+        const items = {
+            firstName : Faker.name.findName(),
+            lastName : Faker.name.findName(),
+            username : Faker.name.findName(),
+            birthDate : Faker.date.between('1985-01-01', '2000-01-05'),
+            email : Faker.internet.email(),
+            city: { connect: random.arrayElement(cities.map((m) => ({id: m.id}))) },
+            sex: 1
+        }
+        promises.push(prisma.user.create({data: items}))
+    }
+ await Promise.all(promises)
+}
