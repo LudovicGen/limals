0182d847802520266cfb9aa6b85618de631eed0e
ludovicGendre <ludovic.gendre@g-mail.fr>
2022-01-07 11:43:04 +0100

    fix: error faker

--- prisma/cities.ts
+++ prisma/cities.ts
@@ -1,21 +1,17 @@
 import * as Faker from 'faker';
-import { City, PrismaClient, User } from "@prisma/client";
-import { CityService } from '../src/user/city/city.service';
-import { random } from 'faker';
-import { CreateUserDto, UserDto } from 'src/dtos';
+import { City, PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
-export const seedCities = async():Promise<void> => {
-    const cities = await prisma.city.findMany()
-    const promises: Promise<City>[] = []
+export const seedCities = async (): Promise<void> => {
+  const promises: Promise<City>[] = [];
 
-    for(let index = 0; index < 10; index++) {
-        const items = {
-            name : Faker.address.city(),
-            postalCode : Faker.address.zipCode(),
-        }
-        promises.push(prisma.city.create({data: items}))
-    }
- await Promise.all(promises)
-}
+  for (let index = 0; index < 10; index++) {
+    const items = {
+      name: Faker.address.city(),
+      postalCode: Faker.address.zipCode(),
+    };
+    promises.push(prisma.city.create({ data: items }));
+  }
+  await Promise.all(promises);
+};
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,13 +1,18 @@
 import { PrismaClient } from '@prisma/client';
+import * as dotenv from 'dotenv';
+import * as Faker from 'faker';
 import { seedUsers } from './users';
 import { seedCities } from './cities';
 
 const prisma = new PrismaClient();
 
 async function main() {
+  dotenv.config();
+  console.log('Seeding...');
   await prisma.user.deleteMany({});
   await prisma.city.deleteMany({});
 
+  console.log('seeding Cities...');
   await seedCities();
 
   console.log('seeding Users...');
