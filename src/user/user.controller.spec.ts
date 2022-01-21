cc6e5fec55fdab7eaec6b0b0fba4f47d0f2b7c87
wewlr17 <gabindepaire@gmail.com>
2022-01-21 14:55:22 +0100

    Add create User test

--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -1,13 +1,35 @@
-import { City } from '@prisma/client';
+import { City, User } from '@prisma/client';
 import { PrismaClient } from '@prisma/client';
+import * as Faker from 'faker';
+import { random } from 'faker';
 
 const prisma = new PrismaClient();
 
 let cityCreate = {} as City;
+let usersList = {} as Promise<User[]>;
+let newUser = {} as User;
+
 beforeAll(async () => {
   //create city
   cityCreate = await prisma.city.create({
     data: { name: 'Bordeaux', postalCode: '33000' },
+  });
+
+  // usersList = prisma.user.findMany();
+  const cities = await prisma.city.findMany();
+
+  newUser = await prisma.user.create({
+    data: {
+      firstName: Faker.name.findName(),
+      lastName: Faker.name.findName(),
+      username: Faker.name.findName(),
+      birthDate: Faker.date.between('1985-01-01', '2000-01-05'),
+      email: Faker.internet.email(),
+      city: { connect: random.arrayElement(cities.map((m) => ({ id: m.id }))) },
+      sex: 1,
+      latitude: 44.837789 + generateRandomFloatInRange(-0.5, 0.5),
+      longitude: -0.57918 + generateRandomFloatInRange(-0.5, 0.5),
+    },
   });
 });
 
@@ -22,4 +44,16 @@ it('Not return object Bordeaux', async () => {
   const newCity = await prisma.city.findFirst();
 
   await expect(newCity).not.toStrictEqual(cityCreate);
-})
+});
+
+it('should return object User', async () => {
+  const data = await prisma.user.findUnique({
+    where: { id: newUser.id },
+  });
+
+  await expect(data).toStrictEqual(newUser);
+});
+
+function generateRandomFloatInRange(min, max) {
+  return Math.random() * (max - min + 1) + min;
+}
